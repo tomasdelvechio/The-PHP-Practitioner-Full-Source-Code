b364e5c26be76e1025d0ceabe8c3f4db0aecab54
Tomas <tomasdelvechio17@gmail.com>
2018-05-19 16:42:04 -0300

    Added logger and singleton method to build logger
    Logger is builded and could be possible provide log level via config file

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -2,9 +2,13 @@
 
 use App\Core\App;
 use App\Core\Database\{QueryBuilder, Connection};
+use App\Core\Logger;
+use Monolog\Logger as MonologLogger;
 
 App::bind('config', require 'config.php');
 
 App::bind('database', new QueryBuilder(
     Connection::make(App::get('config')['database'])
 ));
+
+App::bind('logger', Logger::getLogger(App::get('config')['logger']['level']));
--- index.php
+++ index.php
@@ -3,17 +3,22 @@
 require 'vendor/autoload.php';
 require 'core/bootstrap.php';
 
-use App\Core\{Router, Request};
+use App\Core\{App, Router, Request, Logger};
 use App\Core\Exceptions\RouteNotFoundException as RouteNotFoundException;
+
+$logger = App::get('logger');
 
 try {
 	Router::load('app/routes.php')
 	    ->direct(Request::uri(), Request::method());
+	$logger->info('Status Code: 200');
 } catch (RouteNotFoundException $e) {
 	http_response_code(404);
 	Router::load('app/routes.php')->direct('not_found', 'GET');
+	$logger->debug('Status Code: 404 - Route not found', ["Error" => $e]);
 } catch (Exception $e) {
 	http_response_code(500);
 	Router::load('app/routes.php')->direct('internal_error', 'GET');
+	$logger->error('Status Code: 500 - Internal Server Error', ["Error" => $e]);
 }
 
