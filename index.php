47ba6e0e09dbb8759ef810e34f157b9302d1d688
Tomas <tomasdelvechio17@gmail.com>
2018-05-19 14:52:07 -0300

    Issue #1
    Added handled for basic errors in project

--- index.php
+++ index.php
@@ -4,6 +4,15 @@ require 'vendor/autoload.php';
 require 'core/bootstrap.php';
 
 use App\Core\{Router, Request};
+use App\Core\Exceptions\RouteNotFoundException as RouteNotFoundException;
 
-Router::load('app/routes.php')
-    ->direct(Request::uri(), Request::method());
+try {
+	Router::load('app/routes.php')
+	    ->direct(Request::uri(), Request::method());
+} catch (RouteNotFoundException $e) {
+	http_response_code(404);
+	echo "404 - Not Found";
+} catch (Exception $e) {
+	http_response_code(500);
+	echo "500 - Internal Server Error";
+}
