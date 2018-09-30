d119c8a53453ed8b39c4e7dcaa13b2321959f10a
Tomas <tomasdelvechio17@gmail.com>
2018-09-29 21:00:39 -0300

    some lines deleted

--- core/database/QueryBuilder.php
+++ core/database/QueryBuilder.php
@@ -31,9 +31,7 @@ class QueryBuilder
     public function selectAll($table)
     {
         $statement = $this->pdo->prepare("select * from {$table}");
-
         $statement->execute();
-
         return $statement->fetchAll(PDO::FETCH_CLASS);
     }
 
