8a321daa5e961debc960bb33aa60b70d9a3c93bd
Tomas <tomasdelvechio17@gmail.com>
2018-09-29 21:01:35 -0300

    MVC de Tasks

---
+++ app/models/Task.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+class Task
+{
+    protected $description;
+    protected $completed = false;
+
+    public function __construct($description)
+    {
+        $this->description = $description;
+    }
+
+    public function complete()
+    {
+        $this->completed = true;
+    }
+
+    public function isComplete()
+    {
+        return $this->completed;
+    }
+}
