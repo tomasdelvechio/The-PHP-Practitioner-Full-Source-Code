b208b609ca1fc524187340b70de7d41d29ea6449
Tomas <tomasdelvechio17@gmail.com>
2018-09-29 21:01:16 -0300

    Agregada ruta a tareas

--- app/routes.php
+++ app/routes.php
@@ -1,11 +1,13 @@
  <?php
 
-$router->get('', 'PagesController@home');
-$router->get('about', 'PagesController@about');
-$router->get('contact', 'PagesController@contact');
+    $router->get('', 'PagesController@home');
+    $router->get('about', 'PagesController@about');
+    $router->get('contact', 'PagesController@contact');
 
-$router->get('users', 'UsersController@index');
-$router->post('users', 'UsersController@store');
+    $router->get('users', 'UsersController@index');
+    $router->post('users', 'UsersController@store');
 
-$router->get('not_found', 'ProjectController@notFound');
-$router->get('internal_error', 'ProjectController@internalError');
+    $router->get('tasks', 'TasksController@index');
+
+    $router->get('not_found', 'ProjectController@notFound');
+    $router->get('internal_error', 'ProjectController@internalError');
